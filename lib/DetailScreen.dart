e85720cead1dea7c694c75bfeadf5320f6e8a924
Robby Tan <robby-tan@outlook.com>
2020-02-03 21:38:45 +0700

    Initial commit

---
+++ lib/DetailScreen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:food_recipe/entity/FoodItem.dart';
+
+class DetailScreen extends StatelessWidget {
+    DetailScreen({Key key, this.item}) : super(key: key);
+    final FoodItem item;
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                title: Text('Detail Bahan Makanan'),
+                leading: IconButton(icon:Icon(Icons.arrow_back),
+                    onPressed:() => Navigator.pop(context, false),
+                )
+            ),
+            body: Center(
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    children: <Widget>[
+                      Image.asset(item.imagePath),
+                      Text(item.name),
+                      Text(item.description)
+                    ],
+                  )
+                ),
+            ),
+        );
+    }
+}
---
+++ lib/entity/FoodItem.dart
@@ -0,0 +1,11 @@
+class FoodItem {
+  String name;
+  String description;
+  String imagePath;
+
+  FoodItem(name, description, imagePath) {
+    this.name = name;
+    this.description = description;
+    this.imagePath = imagePath;
+  }
+}
